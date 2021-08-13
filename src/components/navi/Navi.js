b45ccc9c63f505ceadc896927fb30443dc5a57cf
Alperen Kapusuz <sefalperen21@gmail.com>
2021-08-13 15:15:05 +0300

    project setup and install reactstrap

---
+++ src/components/navi/Navi.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+
+const Example = () => {
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">COVID-19</NavbarBrand>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Example;
